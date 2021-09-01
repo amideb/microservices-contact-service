ed57a0dbf5bc33aac88f6d303b4b9704c5cb840f
Debrup Mondal <debrup.official.365@gmail.com>
2021-09-01 10:48:21 +0530

    contact-service initialized

---
+++ src/main/java/com/debrup/microservicescontactservice/MicroservicesContactServiceApplication.java
@@ -0,0 +1,13 @@
+package com.debrup.microservicescontactservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesContactServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesContactServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/debrup/microservicescontactservice/MicroservicesContactServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debrup.microservicescontactservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesContactServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
